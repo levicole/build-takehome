da15e6fa2efcad9c1ba33cf3a9b175fb14f5fe44
Levi Kennedy <lckennedy@gmail.com>
2021-11-20 18:13:43 -0600

    Refactor vegetable method

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -2,14 +2,7 @@ class Vegetable < ApplicationRecord
   has_many :ratings
 
   def average_rating
-    number_of_ratings = ratings.count
-    total = 0
-
-    ratings.each do |rating|
-      total += rating.value
-    end
-
-    total/number_of_ratings
+    ratings.average(:value)
   end
 
   def as_json(options = {})
