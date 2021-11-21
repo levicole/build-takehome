92df5589da0339a20bd6341e55f0eae519f91c78
Levi Kennedy <lckennedy@gmail.com>
2021-11-20 18:12:29 -0600

    Extracts rating method

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -1,15 +1,18 @@
 class Vegetable < ApplicationRecord
   has_many :ratings
 
-  def as_json(options = {})
+  def average_rating
     number_of_ratings = ratings.count
     total = 0
-    
+
     ratings.each do |rating|
       total += rating.value
     end
 
-    average_rating = total/number_of_ratings
+    total/number_of_ratings
+  end
+
+  def as_json(options = {})
     super(options).merge(average_rating: average_rating)
   end
 end
